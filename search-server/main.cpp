82b76400b641f85ac13179718c6d964fa198a61f
иван гаибов <vanyayes@gmail.com>
2022-11-23 18:33:39 +0600

    Fourth sprint

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
-const double setter = 1e-6;
+const double COMPARISON_ACCURACY = 1e-6;
 
 string ReadLine() {
     string s;
@@ -78,28 +78,28 @@ enum class DocumentStatus {
 
 class SearchServer {
 public:
-    inline static constexpr int INVALID_DOCUMENT_ID = -1;
-
+    
     template <typename StringContainer>
     explicit SearchServer(const StringContainer& stop_words)
         : stop_words_(MakeUniqueNonEmptyStrings(stop_words)) {
-            for (const string& wrd : stop_words_){
-            if (!IsValidWord(wrd)){
-                throw invalid_argument("Недопустимые знаки при добавлении стоп-слов"s);
+            for (const string& word : stop_words_){
+                if (!IsValidWord(word)){
+                    throw invalid_argument("Недопустимые знаки при добавлении стоп-слов"s);
+                }
             }
-            }
-    }
+        }
 
     explicit SearchServer(const string& stop_words_text)
-        : SearchServer(SplitIntoWords(stop_words_text)){
-            if (!IsValidWord(stop_words_text)){
-                throw invalid_argument("Недопустимые знаки при добавлении стоп-слов"s);
-            }
-    }
+        : SearchServer(SplitIntoWords(stop_words_text))
+        {    
+        }
 
     void AddDocument(int document_id, const string& document, DocumentStatus status, const vector<int>& ratings) {
-        if (document_id < 0 || documents_.count(document_id) > 0 || !ValidQuery(document)){
+        if (document_id < 0 || documents_.count(document_id) > 0){
             throw invalid_argument("Попытка добавить невалидный документ"s);
+        }
+        if (!IsValidWord(document)){
+            throw invalid_argument("Попытка добавить документ c недопустимыми символами"s);
         }
         const vector<string> words = SplitIntoWordsNoStop(document);
         const double inv_word_count = 1.0 / words.size();
@@ -112,19 +112,14 @@ public:
 
     template <typename DocumentPredicate>
     vector<Document> FindTopDocuments(const string& raw_query,DocumentPredicate document_predicate) const {
-        if (!ValidQuery(raw_query)){
-            throw invalid_argument("Невалидный поисковый запрос"s);
-        }
         const Query query = ParseQuery(raw_query);
-       auto matched_documents = FindAllDocuments(query, document_predicate);
-        sort(matched_documents.begin(), matched_documents.end(),
-             [](const Document& lhs, const Document& rhs) {
-                 if (abs(lhs.relevance - rhs.relevance) < setter) {
-                     return lhs.rating > rhs.rating;
-                 } else {
-                     return lhs.relevance > rhs.relevance;
-                 }
-             });
+        auto matched_documents = FindAllDocuments(query, document_predicate);
+        sort(matched_documents.begin(), matched_documents.end(),[](const Document& lhs, const Document& rhs) {
+            if (abs(lhs.relevance - rhs.relevance) < COMPARISON_ACCURACY) {
+                return lhs.rating > rhs.rating;
+            } else {
+                return lhs.relevance > rhs.relevance;
+            }});
         if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
             matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
         }        
@@ -144,10 +139,7 @@ public:
     }
 
    tuple<vector<string>,DocumentStatus> MatchDocument(const string& raw_query, int document_id) const {
-        if (!ValidQuery(raw_query)){
-            throw invalid_argument("Невалидный поисковый запрос"s);
-        }
-      const Query query = ParseQuery(raw_query);
+        const Query query = ParseQuery(raw_query);
         vector<string> matched_words;
         for (const string& word : query.plus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
@@ -173,7 +165,7 @@ public:
        if (index < 0 || index > documents_.size()){
            throw out_of_range("Нет документа с таким индексом"s);
        }
-    return all_documents_[index];
+        return all_documents_[index];
     }
 
 private:
@@ -181,12 +173,12 @@ private:
         int rating;
         DocumentStatus status;
     };
+    
     const set<string> stop_words_;
     map<string, map<int, double>> word_to_document_freqs_;
     map<int, DocumentData> documents_;
     vector<int> all_documents_;
    
-
     bool IsStopWord(const string& word) const {
         return stop_words_.count(word) > 0;
     }
@@ -225,6 +217,9 @@ private:
             is_minus = true;
             text = text.substr(1);
         }
+        if (text.empty() || text[0] == '-'){
+            throw invalid_argument("Невалидный поисковый запрос"s);
+        }
         return {text, is_minus, IsStopWord(text)};
     }
 
@@ -236,6 +231,9 @@ private:
     Query ParseQuery(const string& text) const {
         Query query;
         for (const string& word : SplitIntoWords(text)) {
+            if (!IsValidWord(word)){
+                throw invalid_argument("В поисковом запросе недопустимые символы"s);
+                }
             const QueryWord query_word = ParseQueryWord(word);
             if (!query_word.is_stop) {
                 if (query_word.is_minus) {
@@ -292,22 +290,6 @@ private:
             return c >= '\0' && c < ' ';
         });
     }
-    
-    static bool ValidQuery (const string& query){
-        vector<string> words = SplitIntoWords(query);
-            for (const auto& word : words){
-                if (!IsValidWord(word)){
-                    return false;
-                }
-                if (word.size() == 1 && word[0] == '-'){
-                    return false;
-                }
-                if (word[0] == '-' && word [1] == '-'){
-                    return false;
-                }
-            }
-        return true;
-    }
 };
 
 // ==================== для примера =========================
@@ -336,6 +318,5 @@ int main() {
     cout << "invalid_argument: "s << e.what() << endl; 
     }catch (const out_of_range& e) {
         cout << "out_of_ranget: "s << e.what() << endl; 
-    }
-    
+    }    
 }
